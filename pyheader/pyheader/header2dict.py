60bc811012a8979adfa09cea4d431161706aa820
leegohi <walle88@qq.com>
2018-07-03 15:47:32 +0800

    repair encode bugs

--- pyheader/pyheader/header2dict.py
+++ pyheader/pyheader/header2dict.py
@@ -34,25 +34,25 @@ def get_clip():
     if WIN:
         wc.OpenClipboard()
         try:
-            copy_text = wc.GetClipboardData(win32con.CF_UNICODETEXT)
+            copy_text = str(wc.GetClipboardData(win32con.CF_UNICODETEXT))
         except:
             traceback.print_exc()
         finally:
             wc.CloseClipboard()
-
-        return copy_text
-    p = subprocess.Popen(['pbpaste'], stdout=subprocess.PIPE)
-    retcode = p.wait()
-    copy_text = p.stdout.read().decode("utf-8")
+        copy_text= copy_text
+    if not copy_text:
+        p = subprocess.Popen(['pbpaste'], stdout=subprocess.PIPE)
+        retcode = p.wait()
+        copy_text = p.stdout.read()
     return copy_text
 
 
 def get_headers():
     copy_text = get_clip()
+    print "RAW DATA:\n",copy_text
     req = copy_text.split('\r')
     if len(req) == 1:
         req = copy_text.split("\n")
-    print "RAW DATA:\n",req
     head = {}
     res=[]
     line1 = req[0]
@@ -66,9 +66,8 @@ def get_headers():
             traceback.print_exc()
             if "&" in lastline:
                 param = unquote(lastline.strip())
-                print param
                 data = dict(map(lambda i: i.split("=", 1), param.split("&")))
-    qstr = splitquery(line1.split(" ")[1])[1]
+    path,qstr = splitquery(line1.split(" ")[1])
     if qstr:
         param = unquote(qstr)
         data = dict(map(lambda i: i.split("=", 1), param.split("&")))
@@ -85,12 +84,34 @@ def get_headers():
         head[temp[0]] = temp[1]
     res.append("HEADERS:")
     res.append(json.dumps(head, indent=1, ensure_ascii=False))
+    res.append("URL:")
+    if path.startswith("https://") or path.startswith("http://"):
+        res.append(path)
+    else:
+        res.append(head["Host"]+path)
     res_str="\n".join(res)
-    print res_str
+    decode_res=res_str.decode("utf-8")
+    print decode_res
     #重新转成bytes型
-    set_clip(res_str)
+    set_clip(decode_res)
 
 def main():
-    get_headers()
+    import warnings
+    try:
+        get_headers()
+    except:
+        traceback.print_exc()
+        warnings.warn("""
+        You should copy right HTTP form string from fiddler,chrome console or any other editor. 
+        Example:        
+            GET https://www.fiddler2.com/UpdateCheck.aspx?isBeta=False HTTP/1.1
+            User-Agent: Fiddler/4.6.20173.38786 (.NET 4.0; WinNT 6.1.7601 SP1;)
+            Pragma: no-cache
+            Host: www.fiddler2.com
+            Accept-Language: zh-CN
+            Referer: http://fiddler2.com/client/4.6.20173.38786
+            Accept-Encoding: gzip, deflate
+            Connection: close
+        """)
 if __name__ == "__main__":
     main()
